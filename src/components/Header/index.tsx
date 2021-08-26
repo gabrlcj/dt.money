0e8510a650652d9de264132955945ed576e100f8
Gabriel <gabrielbittencourt57@gmail.com>
2021-08-26 17:05:04 -0300

    :package: Components folder

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import LogoImg from '../../assets/logo.svg'
+import { Container, Content } from './styles'
+
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+  return (
+    <Container>
+      <Content>
+        <img src={LogoImg} alt="logo do projeto" />
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
+      </Content>
+    </Container>
+  )
+}
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./styles"
+import IncomeImg from '../../assets/income.svg'
+import OutcomeImg from '../../assets/outcome.svg'
+import TotalImg from '../../assets/total.svg'
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={IncomeImg} alt="Icone de entradas" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={OutcomeImg} alt="Icone de saídas" />
+        </header>
+        <strong>-R$500,00</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={TotalImg} alt="Icone de total" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+    </Container>
+  )
+}
